64a043dda9fdb99dd49c38811cb7d5d697c1b2eb
fuad <m.fuadyerdelen@gmail.com>
2021-09-21 14:29:50 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+
+
+function DenemeTitle() {
+    return (
+        <h2>naber la</h2>
+    )
+}
+
+ReactDOM.render( <DenemeTitle/>  , document.getElementById('root'));
+
+
+
